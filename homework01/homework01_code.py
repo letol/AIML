cb375c91a438ebde4b6feabd6cedbed3ff60462d
letol <tolomei.leonardo@gmail.com>
2019-11-14 16:17:59 +0100

    HW1 - corrected training on train+val

--- homework01/homework01_code.py
+++ homework01/homework01_code.py
@@ -11,12 +11,12 @@ print("\n\t- Initialization")
 
 all_features, target = load_wine(return_X_y=True)
 features = all_features[:, :2]
-features_train, features_test, target_train, target_test = train_test_split(features, target, test_size=.3)
-features_train, features_val, target_train, target_val = train_test_split(features_train, target_train,
+features_train_val, features_test, target_train_val, target_test = train_test_split(features, target, test_size=.3)
+features_train, features_val, target_train, target_val = train_test_split(features_train_val, target_train_val,
                                                                           test_size=(2 / 7))
 
 # initializing the random seed for better debugging
-# np.random.seed(0)
+np.random.seed(1)
 
 # step size in the mesh
 h = .02
@@ -37,17 +37,16 @@ print("\n\t- K-Nearest Neighbor")
 
 K = [1, 3, 5, 7]
 
-# initialize classifiers and accuracies lists
-clf1 = []
+# initialize accuracies list
 accuracy = []
 
 for i, k in enumerate(K):
     # Apply k-NN
-    clf1.append(KNeighborsClassifier(n_neighbors=k))
-    clf1[i].fit(features_train, target_train)
+    model = KNeighborsClassifier(n_neighbors=k)
+    model.fit(features_train, target_train)
 
     # Plot the decision boundaries
-    Z = clf1[i].predict(np.c_[x.ravel(), y.ravel()])
+    Z = model.predict(np.c_[x.ravel(), y.ravel()])
     Z = Z.reshape(x.shape)
     plt.pcolormesh(x, y, Z, cmap=cmap_light)
 
@@ -60,7 +59,7 @@ for i, k in enumerate(K):
     plt.show()
 
     # Evaluate on the validation set
-    accuracy.append(clf1[i].score(features_val, target_val))
+    accuracy.append(model.score(features_val, target_val))
 
 plt.ylim(0.5, 1)
 plt.ylabel("accuracy")
@@ -70,7 +69,9 @@ plt.title("k-NN accuracy on validation set")
 plt.show()
 
 best_K_index = np.argmax(accuracy)
-accuracy_test = clf1[best_K_index].score(features_test, target_test)
+clf1 = KNeighborsClassifier(n_neighbors=K[best_K_index])
+clf1.fit(features_train_val, target_train_val)
+accuracy_test = clf1.score(features_test, target_test)
 print("Best K is %i and corresponding accuracy on test set is %f%%" % (K[best_K_index], accuracy_test * 100))
 
 # %% Linear SVM
@@ -78,17 +79,16 @@ print("\n\t- Linear SVM")
 
 C = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
 
-# initialize classifiers and accuracies lists
-clf2 = []
+# initialize accuracies list
 accuracy.clear()
 
 for i, c in enumerate(C):
     # Apply Linear SVM
-    clf2.append(LinearSVC(C=c))
-    clf2[i].fit(features_train, target_train)
+    model = LinearSVC(C=c)
+    model.fit(features_train, target_train)
 
     # Plot the decision boundaries
-    Z = clf2[i].predict(np.c_[x.ravel(), y.ravel()])
+    Z = model.predict(np.c_[x.ravel(), y.ravel()])
     Z = Z.reshape(x.shape)
     plt.pcolormesh(x, y, Z, cmap=cmap_light)
 
@@ -101,7 +101,7 @@ for i, c in enumerate(C):
     plt.show()
 
     # Evaluate on the validation set
-    accuracy.append(clf2[i].score(features_val, target_val))
+    accuracy.append(model.score(features_val, target_val))
 
 plt.ylim(0, 1)
 plt.ylabel("accuracy")
@@ -111,23 +111,24 @@ plt.title("Linear SVM accuracy on validation set")
 plt.show()
 
 best_C_index_1 = np.argmax(accuracy)
-accuracy_test = clf2[best_C_index_1].score(features_test, target_test)
+clf2 = LinearSVC(C=C[best_C_index_1])
+clf2.fit(features_train_val, target_train_val)
+accuracy_test = clf2.score(features_test, target_test)
 print("Best C is %f and corresponding accuracy on test set is %f%%" % (C[best_C_index_1], accuracy_test * 100))
 
 # %% SVM with RBF kernel
 print("\n\t- SVM with RBF kernel")
 
-# initialize classifiers and accuracies lists
-clf3 = []
+# initialize accuracies list
 accuracy.clear()
 
 for i, c in enumerate(C):
     # Apply SVM
-    clf3.append(SVC(C=c, gamma='auto', kernel='rbf'))
-    clf3[i].fit(features_train, target_train)
+    model = SVC(C=c, gamma='auto', kernel='rbf')
+    model.fit(features_train, target_train)
 
     # Plot the decision boundaries
-    Z = clf3[i].predict(np.c_[x.ravel(), y.ravel()])
+    Z = model.predict(np.c_[x.ravel(), y.ravel()])
     Z = Z.reshape(x.shape)
     plt.pcolormesh(x, y, Z, cmap=cmap_light)
 
@@ -140,7 +141,7 @@ for i, c in enumerate(C):
     plt.show()
 
     # Evaluate on the validation set
-    accuracy.append(clf3[i].score(features_val, target_val))
+    accuracy.append(model.score(features_val, target_val))
 
 plt.ylim(0, 1)
 plt.ylabel("accuracy")
@@ -150,7 +151,9 @@ plt.title("SVM with RBF kernel accuracy on validation set")
 plt.show()
 
 best_C_index_2 = np.argmax(accuracy)
-accuracy_test = clf3[best_C_index_2].score(features_test, target_test)
+clf3 = SVC(C=C[best_C_index_2], gamma='auto', kernel='rbf')
+clf3.fit(features_train_val, target_train_val)
+accuracy_test = clf3.score(features_test, target_test)
 print("Best C is %f and corresponding accuracy on test set is %f%%" % (C[best_C_index_2], accuracy_test * 100))
 
 # %% SVM with RBF kernel and grid search
@@ -166,7 +169,7 @@ n_C = 7
 
 param_grid = {'C': np.logspace(C_min, C_max, n_C), 'gamma': np.logspace(gamma_min, gamma_max, n_g), 'kernel': ['rbf']}
 best_score = 0
-best_params = {'C': C_min, 'gamma': gamma_min}
+best_params = {'C': C_min, 'gamma': gamma_min, 'kernel': 'rbf'}
 
 for params in ParameterGrid(param_grid):
     model = SVC(**params)
@@ -175,9 +178,10 @@ for params in ParameterGrid(param_grid):
     if score > best_score:
         best_score = score
         best_params = params
-        clf4 = model
 
 # Plot the decision boundaries
+clf4 = SVC(**best_params)
+clf4.fit(features_train_val, target_train_val)
 Z = clf4.predict(np.c_[x.ravel(), y.ravel()])
 Z = Z.reshape(x.shape)
 plt.pcolormesh(x, y, Z, cmap=cmap_light)
@@ -196,8 +200,5 @@ print("Best params are C=%f and gamma=%f and corresponding accuracy on test set 
 
 # %% K-Fold
 print("\n\t- K-Fold")
-
-features_train = np.concatenate((features_train, features_val))
-target_train = np.concatenate((target_train, target_val))
 
 
